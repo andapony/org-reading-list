46c810824bee13c8595e167a862c5945dc6ba9e0
Rob Duncan <andapony@robduncan.info>
2026-06-12 18:02:03 -0700

    Document :rev :newest install, bump version to 0.4.1

--- org-reading-list.el
+++ org-reading-list.el
@@ -4,7 +4,7 @@
 
 ;; Author: Rob Duncan
 ;; URL: https://github.com/YOUR-USERNAME/org-reading-list
-;; Version: 0.4.0
+;; Version: 0.4.1
 ;; Package-Requires: ((emacs "27.1") (org "9.4"))
 ;; Keywords: bib, outlines
 ;; SPDX-License-Identifier: GPL-3.0-or-later
