e75bab5771e0388cffb187e5b47637d575968edb
Rob Duncan <andapony@robduncan.info>
2026-06-27 02:25:55 +0000

    chore: bump version to 0.11.0
    Release the import-heading feature: a core org-reading-list-import
    command that copies an entry (or every entry) from another reading-list
    file into the main list. Bump all three module headers to 0.11.0.

--- org-reading-list-mi.el
+++ org-reading-list-mi.el
@@ -4,7 +4,7 @@
 
 ;; Author: Rob Duncan
 ;; URL: https://github.com/andapony/org-reading-list
-;; Version: 0.10.0
+;; Version: 0.11.0
 ;; Package-Requires: ((emacs "28.1") (org "9.4"))
 ;; Keywords: bib, outlines
 ;; SPDX-License-Identifier: GPL-3.0-or-later
